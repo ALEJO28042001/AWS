e31edee6e98110ab20b4df034b0cc77bd2ae6e3e
Ubuntu <ubuntu@ip-172-31-63-15.ec2.internal>
2023-02-23 19:54:38 +0000

    final

--- test_funciones.py
+++ test_funciones.py
@@ -1,11 +1,11 @@
-from funciones import *
-def test_sumar(x,y):
+from funciones import sumar,es_primo
+def test_sumar():
     assert sumar(2,5) == 7
     assert sumar(2,2) == 4
     assert sumar(1,2) == 3
     
 def test_es_primo():
-    assert es_primo(4) == True
-    assert es_primo(5) == True
-    assert es_primo(9) == True
-    assert es_primo(7) == True
+    assert es_primo(4) is False
+    assert es_primo(5) is True
+    assert es_primo(9) is False
+    assert es_primo(7) is True
